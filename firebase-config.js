1e1b0b00d157f55115244ae33e16fb14c8787389
wphanich <wphanich@gmail.com>
2026-09-11 05:45:35 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// แก้ค่าเหล่านี้จาก Firebase Console > Project settings > Your apps > Web app
+
+  const firebaseConfig = {
+  apiKey: "AIzaSyDyp5jxnd7S9rcrS8iOsTnSuX2dZ9Y1aQM",
+  authDomain: "retirement-budget-e66b8.firebaseapp.com",
+  projectId: "retirement-budget-e66b8",
+  storageBucket: "retirement-budget-e66b8.firebasestorage.app",
+  messagingSenderId: "1048564526868",
+  appId: "1:1048564526868:web:8b72688ac3053a36529c01",
+  measurementId: "G-0150D2ZGRQ"
+};
+
+
+// รหัสงาน — เปลี่ยนได้ถ้าต้องการแยกงานหลายปี เช่น "retire-2570"
+
+export const EVENT_ID = "retire-2569";
