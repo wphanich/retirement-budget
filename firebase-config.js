66c4d6506f02d69af05f88288fdec6b94ba65b39
wphanich <wphanich@gmail.com>
2026-09-11 06:06:47 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 // แก้ค่าเหล่านี้จาก Firebase Console > Project settings > Your apps > Web app
 
-  const firebaseConfig = {
+  export const firebaseConfig = {
   apiKey: "AIzaSyDyp5jxnd7S9rcrS8iOsTnSuX2dZ9Y1aQM",
   authDomain: "retirement-budget-e66b8.firebaseapp.com",
   projectId: "retirement-budget-e66b8",
